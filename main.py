4cfeda579bd31838903b23ba1786f1f3e06cb24c
Wolkar <wolkar25@gmail.com>
2025-07-17 11:36:31 +0200

    D: as wrid jetzt sortiert

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ def get_book_text(file_path):
         book_text = book.read()
         return book_text
 from stats import word_count
+from stats import symbol_count
 def main(file_path):
     book_text = get_book_text(file_path)
     word_number = word_count(book_text)
+    symbols = symbol_count(book_text)
     print(f"{word_number} words found in the document")
-
+    print(symbols)
 file_path = "books/frankenstein.txt"
 main(file_path)
--- stats.py
+++ stats.py
@@ -2,6 +2,10 @@ def word_count(book_text):
     words = book_text.split()
     return len(words) 
 def symbol_count(book_text):
-    words = book_text.split()
-    symbols = words.split()
-    print(symbols)
+    symbols = {}
+    book_text = book_text.lower()
+    for letter in book_text:
+        if letter not in symbols:
+            symbols[letter] = 0
+        symbols[letter] += 1
+    return symbols
