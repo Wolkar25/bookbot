ff4e0d70ee6ba193acba40a7bcc1b98db36a39b3
Wolkar <wolkar25@gmail.com>
2025-07-20 08:14:20 +0200

    E: s ist bald fertig

--- main.py
+++ main.py
@@ -5,11 +5,23 @@ def get_book_text(file_path):
         return book_text
 from stats import word_count
 from stats import symbol_count
+from stats import sort_symbols
+def alphafinder(symbols):
+    for symbol in symbols:
+        char = symbol["char"]
+        if char.isalpha() == True:
+            print(f"{symbol["char"]}: {symbol["num"]}")
 def main(file_path):
     book_text = get_book_text(file_path)
     word_number = word_count(book_text)
-    symbols = symbol_count(book_text)
-    print(f"{word_number} words found in the document")
-    print(symbols)
+    symbols_unsorted = symbol_count(book_text)
+    symbols = sort_symbols(symbols_unsorted)
+    print("============ BOOKBOT ============ \n"
+          f"Analyzing book found at {file_path}... \n "
+          "----------- Word Count ---------- \n "
+          f"Found {word_number} total words \n "
+          "--------- Character Count ------- \n ")
+    alphafinder(symbols)
+    print("============= END ===============")
 file_path = "books/frankenstein.txt"
 main(file_path)
--- stats.py
+++ stats.py
@@ -9,3 +9,17 @@ def symbol_count(book_text):
             symbols[letter] = 0
         symbols[letter] += 1
     return symbols
+def sort_on(sort_list):
+    return sort_list["num"]
+def sort_symbols(symbols_unsorted):
+    sort_list = []
+    for pair in symbols_unsorted:
+        sort_list.append({"char" : pair, "num" : symbols_unsorted[pair]})
+    sort_list.sort(reverse=True, key=sort_on)
+    return sort_list
+    
+
+
+book_text = "Ladies and Gentlemen"
+symbols_unsorted = symbol_count(book_text)
+sort_symbols(symbols_unsorted)
